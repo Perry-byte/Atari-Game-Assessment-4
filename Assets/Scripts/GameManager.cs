b3cc8784f553cd6d2b4ee92d46f964c38ec664aa
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-10-08 21:42:39 +1100

    Level Buttons Created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState { Start, LevelOne };
+
+    public static GameState currentGameState = GameState.Start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
