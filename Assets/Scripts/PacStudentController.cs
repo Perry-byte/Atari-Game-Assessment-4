411df3c3ae09ef1a3e4bd5f97bb6f443e26dd997
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-10-19 23:41:54 +1100

    Collisions Updated

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
-    Vector3 lastInput;
+    Vector3 lastInput = new Vector3(0.0f,0.0f,0.0f);
+    Vector3 currentInput = new Vector3(0.0f, 0.0f, 0.0f);
     [SerializeField]
     Tweener tweener;
     // Start is called before the first frame update
@@ -16,31 +17,65 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("a")) {
-            lastInput = gameObject.transform.position + new Vector3(-0.35f, 0.0f, 0.0f);
-            if (checkCollision()) {
+        if (Input.GetKeyDown("a"))
+        {
+            currentInput = gameObject.transform.position + new Vector3(-0.36f, 0.0f, 0.0f);
+            if (checkCollision(currentInput))
+            {
                 Debug.Log("stop");
-            } else
+            }
+            else
             {
                 tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(-0.36f, 0.0f, 0.0f), 0.5f);
             }
+            lastInput = gameObject.transform.position + new Vector3(-0.36f, 0.0f, 0.0f);
         }
-        if (Input.GetKeyDown("d"))
+        else if (Input.GetKeyDown("d"))
         {
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.36f, 0.0f, 0.0f), 0.5f);
+            currentInput = gameObject.transform.position + new Vector3(0.36f, 0.0f, 0.0f);
+            if (checkCollision(currentInput))
+            {
+                Debug.Log("stop");
+            }
+            else
+            {
+                tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.36f, 0.0f, 0.0f), 0.5f);
+            }
+            lastInput = gameObject.transform.position + new Vector3(0.36f, 0.0f, 0.0f);
         }
-        if (Input.GetKeyDown("w"))
+        else if (Input.GetKeyDown("w"))
         {
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, 0.36f, 0.0f), 0.5f);
+            currentInput = gameObject.transform.position + new Vector3(0.0f, 0.36f, 0.0f);
+            if (checkCollision(currentInput))
+            {
+                Debug.Log("stop");
+            }
+            else
+            {
+                tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, 0.36f, 0.0f), 0.5f);
+            }
+            lastInput = gameObject.transform.position + new Vector3(0.0f, 0.36f, 0.0f);
         }
-        if (Input.GetKeyDown("s"))
+        else if (Input.GetKeyDown("s"))
         {
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, -0.36f, 0.0f), 0.5f);
+            currentInput = gameObject.transform.position + new Vector3(0.0f, -0.36f, 0.0f);
+            if (checkCollision(currentInput))
+            {
+                Debug.Log("stop");
+            }
+            else
+            {
+                tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, -0.36f, 0.0f), 0.5f);
+            }
+            lastInput = gameObject.transform.position + new Vector3(0.0f, -0.36f, 0.0f);
+        }
+        else if (checkCollision(lastInput)) { 
+            
         }
     }
 
-    bool checkCollision() {
-        if (lastInput.x < -3.75 || (lastInput.x > -3.66 && lastInput.x < -1.79 && lastInput.y < 4.9 && lastInput.y > 3.8))
+    bool checkCollision(Vector3 move) {
+        if (move.x < -3.75 || (move.x > -3.66 && move.x < -1.79 && move.y < 4.9 && move.y > 3.8))
         {
             return true;
         }
