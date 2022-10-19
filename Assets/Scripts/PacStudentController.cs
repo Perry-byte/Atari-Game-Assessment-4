edcfa9125b1cb58ec954aa62b704248dadac74d6
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-10-19 22:33:03 +1100

    First Collisions Implemented

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
-    Input lastInput;
+    KeyCode lastInput;
+    Vector3 move;
     [SerializeField]
     Tweener tweener;
     // Start is called before the first frame update
@@ -17,19 +18,37 @@ public class PacStudentController : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown("a")) {
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(-0.35f,0.0f,0.0f), 1.5f);
+            move = gameObject.transform.position + new Vector3(-0.35f, 0.0f, 0.0f);
+            if (checkCollision()) {
+                Debug.Log("stop");
+            } else
+            {
+                tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(-0.36f, 0.0f, 0.0f), 0.5f);
+                lastInput = KeyCode.A;
+            }
         }
         if (Input.GetKeyDown("d"))
         {
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.35f, 0.0f, 0.0f), 1.5f);
+            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.36f, 0.0f, 0.0f), 0.5f);
+            lastInput = KeyCode.D;
         }
         if (Input.GetKeyDown("w"))
         {
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, 0.35f, 0.0f), 1.5f);
+            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, 0.36f, 0.0f), 0.5f);
+            lastInput = KeyCode.W;
         }
         if (Input.GetKeyDown("s"))
         {
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, -0.35f, 0.0f), 1.5f);
+            tweener.AddTween(gameObject.transform, gameObject.transform.position, gameObject.transform.position + new Vector3(0.0f, -0.36f, 0.0f), 0.5f);
+            lastInput = KeyCode.S;
         }
+    }
+
+    bool checkCollision() {
+        if (move.x < -3.75 || (move.x > -3.66 && move.x < -1.79 && move.y < 4.9 && move.y > 3.8))
+        {
+            return true;
+        }
+        return false;
     }
 }
